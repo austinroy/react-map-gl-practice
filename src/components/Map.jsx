e0720d2d382f96e54b61e66daa8fa2e442ed643c
Austin Roy <austin.roy@andela.com>
2019-06-26 02:04:23 +0300

    Create Map with Geolocator

---
+++ src/components/Map.jsx
@@ -0,0 +1,46 @@
+import React,{ useState, useRef } from 'react'
+import MapGL, {GeolocateControl } from 'react-map-gl'
+import config from '../config'
+import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+const TOKEN=config.REACT_APP_TOKEN
+
+const geolocateStyle = {
+  float: 'left',
+  margin: '50px',
+  padding: '10px'
+};
+
+const Map = () => {
+
+  const [viewport, setViewPort ] = useState({
+    width: "100%",
+    height: 900,
+    latitude: 0,
+    longitude: 0,
+    zoom: 2
+  })
+
+  const _onViewportChange = viewport => setViewPort({...viewport, transitionDuration: 3000 })
+  
+  return (
+    <div style={{ margin: '0 auto'}}>
+      <h1 style={{textAlign: 'center', fontSize: '25px', fontWeight: 'bolder' }}>GeoLocator: Click To Find Your Location</h1>
+      <MapGL
+        {...viewport}
+        mapboxApiAccessToken={TOKEN}
+        mapStyle="mapbox://styles/mapbox/dark-v8"
+        onViewportChange={_onViewportChange}
+      >
+        <GeolocateControl
+          style={geolocateStyle}
+          positionOptions={{enableHighAccuracy: false}}
+          trackUserLocation={true}
+        />
+      </MapGL>
+    </div>
+  )
+}
+
+export default Map
---
+++ src/config.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+const env = {
+    REACT_APP_TOKEN: process.env.REACT_APP_TOKEN,
+}
+
+export default env;
